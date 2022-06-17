d136b2ff78fcd53a2f59e4bca899d35d251406c8
Inna <Innnusik555@mail.ru>
2022-06-17 13:50:36 +0600

    add images

---
+++ index.js
@@ -0,0 +1,108 @@
+/*let admin;
+let name;
+
+name= 'Джон';
+admin = name;
+alert(admin);
+
+
+
+let planet = 'Life';
+let nowVisitor = 'Client';
+
+
+
+let name = 'Ilya';
+
+alert(`hello ${1}`);  // hello 1
+alert(`hello ${"name"}`);  // hello name
+alert(`hello ${name}`);  // hello Ilya
+
+
+
+let name = prompt('Введите имя?', ' ');
+
+alert(name); 
+
+
+
+console.log( ''+ 1 + 0);  // 10
+console.log( ''- 1 + 0);  // -1
+console.log( true + false);  // 1
+console.log( 6/ '3');  // 2
+console.log( '2' * '3');  // 6
+console.log( 4 + 5 + 'px');  // 9px
+console.log( '$' + 4 + 5);  // $45
+console.log( '4' - 2);  // 2
+console.log( '4px' - 2);  // NaN
+console.log( -9 + 5);  // -4
+console.log( -9 - 5);  // -14
+console.log( null + 1);  // 1
+console.log( undefined + 1);  // NaN
+console.log( '\t\n' - 2);  // -2
+
+
+
+let a=+prompt('Первое число?',1);
+let b=+prompt('Второе число?',2);
+
+alert(a + b); // 3
+
+
+
+let num= 3;
+alert(num);
+
+
+
+let a= 10;
+let b= 2;
+
+alert(a + b);
+alert(a - b);
+alert(a * b);
+alert(a / b);
+
+
+
+let c= 15;
+let d= 2;
+result= c + d;
+alert(result);
+
+
+
+let a= 10;
+let b= 2;
+let c= 5;
+alert(a + b + c);
+
+
+
+let a= 17;
+let b= 10;
+let c= a - b;
+let d= 7;
+result=c + d;
+alert(result);
+
+
+
+
+let a= 7;
+result=a ** 2;
+alert(result);
+
+
+
+let a= 60; // секунды
+let b= 60; //минуты
+let c= a * b; // в часе секунд
+alert(a * b); // 3600 
+let d= 24; // часов в сутках
+let e= c * d; // секунд в сутках
+alert(c * d); // 86400
+let f= 30; // дней в месяце
+let g= e * f; // секунд в месяце
+alert(e * f); // 2592000 секунд в месяце
+*/
